84ec4a21498f2d21ad3362e5d4946af277645ca5
Joao Bruno <jbrunomf@outlook.com>
2023-11-20 11:36:48 -0400

    first commit

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import type { PropType } from 'vue';
+import type ICategoria from '@/interfaces/ICategoria';
+
+    export default {
+        props: {
+            categoria: { type: Object as PropType<ICategoria>, required: true }
+        }
+    }
+</script>
+
+<template>
+    <article class="categoria">
+        <header class="categoria__cabecalho">
+            <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
+        </header>
+    </article>
+</template>
